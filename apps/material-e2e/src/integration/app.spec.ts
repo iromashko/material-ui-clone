3657124b89cb925151121a8b5fc4f99f311e671b
iromashko <iromashko@me.com>
2021-07-15 15:53:51 +0300

    progress bar test

--- apps/material-e2e/src/integration/app.spec.ts
+++ apps/material-e2e/src/integration/app.spec.ts
@@ -1,6 +1,14 @@
 describe('Material Components', () => {
   beforeEach(() => cy.visit('/'));
 
+  it('Progress Bar', () => {
+    cy.get('[data-cy=progress-value]').contains(25);
+    cy.get('[data-cy=progress-update]').click();
+    cy.get('[data-cy=progress-value]').contains(100);
+    cy.get('[data-cy=progress-reset]').click();
+    cy.get('[data-cy=progress-value]').contains(0);
+  });
+
   it('Accordion Component', () => {
     cy.get('[data-cy=accordion-header]').should('have.length', 2);
     cy.get('[data-cy=accordion-header] > i').should(
