aeab65ad038d1a3b9c5d03f0da5407c09c471458
iromashko <iromashko@me.com>
2021-07-15 09:25:51 +0300

    toolbar

--- apps/showcase/src/app/app.module.ts
+++ apps/showcase/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { SharedUiToolbarModule } from '@material-uiclone/shared/ui-toolbar';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    SharedUiToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ libs/shared/ui-toolbar/README.md
@@ -0,0 +1,7 @@
+# shared-ui-toolbar
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test shared-ui-toolbar` to execute the unit tests.
