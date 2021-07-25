5eddcf0e12fa0a3244ae97b9c685e1525263a006
iromashko <iromashko@me.com>
2021-07-25 07:16:07 +0300

    refactor theme

--- libs/shared/ui-material/src/lib/tab/tab.component.ts
+++ libs/shared/ui-material/src/lib/tab/tab.component.ts
@@ -4,6 +4,7 @@ import {
   Input,
   Output,
   EventEmitter,
+  ViewEncapsulation,
 } from '@angular/core';
 
 export interface TabItem {
@@ -16,6 +17,7 @@ export interface TabItem {
   templateUrl: './tab.component.html',
   styleUrls: ['./tab.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
 })
 export class TabComponent {
   @Input() tabs: TabItem[] = [];
