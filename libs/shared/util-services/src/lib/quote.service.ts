7e56bce36c296b2880da1ff122201615a0a45917
iromashko <iromashko@me.com>
2021-07-25 23:12:47 +0300

    theme refactor

--- libs/shared/util-services/src/lib/quote.service.ts
+++ libs/shared/util-services/src/lib/quote.service.ts
@@ -1,13 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { catchError, map, shareReplay } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class QuoteService {
   randomQuote$ = this.http
     .get<string[][]>('http://www.famous-quotes.uk/api.php?id=random&minpop=80')
     .pipe(
-      map((response) => ({ quote: response[0][1], author: response[0][2] }))
+      map((response) => ({ quote: response[0][1], author: response[0][2] })),
+      catchError(() => of({ quote: 'Not found', author: 'Connection error' })),
+      shareReplay(1)
     );
 
   constructor(private http: HttpClient) {}
