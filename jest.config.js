7ef16d83f4db02ee13ce0f360532ff95706e354b
iromashko <iromashko@me.com>
2021-07-26 22:56:34 +0300

    update angular

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,3 @@
-module.exports = {
-  projects: [
-    ,
-    '<rootDir>/libs/shared/ui-material',
-    '<rootDir>/libs/material/domain',
-    '<rootDir>/libs/material/feature-components',
-    '<rootDir>/libs/material/feature-directives',
-    '<rootDir>/libs/material/feature-services',
-    '<rootDir>/libs/material/feature-pipes',
-    '<rootDir>/libs/material/feature-other',
-    '<rootDir>/libs/shared/ui-pipes',
-    '<rootDir>/libs/shared/ui-directives',
-    '<rootDir>/libs/shared/util-services',
-    '<rootDir>/libs/shared/util-helpers',
-    '<rootDir>/libs/shared/ui-animations',
-    '<rootDir>/libs/shared/util-guards',
-  ],
-};
+const { getJestProjects } = require('@nrwl/jest');
+
+module.exports = { projects: getJestProjects() };
