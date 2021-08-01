32d3a46715d8e67213324cd961c2f4710c822864
iromashko <iromashko@me.com>
2021-08-01 06:09:24 +0300

    countdown refactor

--- libs/shared/ui-material/src/lib/countdown-timer/countdown-timer.component.ts
+++ libs/shared/ui-material/src/lib/countdown-timer/countdown-timer.component.ts
@@ -4,6 +4,12 @@ import {
   Component,
   Input,
 } from '@angular/core';
+import {
+  differenceInDays,
+  differenceInHours,
+  differenceInMinutes,
+  differenceInSeconds,
+} from 'date-fns';
 
 @Component({
   selector: 'material-countdown-timer',
@@ -13,68 +19,28 @@ import {
 export class CountdownTimerComponent implements AfterViewInit {
   countDownTime!: number;
 
-  timerNumerics = {
-    milliseconds: 1000,
-    minuteInSeconds: 60,
-    minutesInHours: 60,
-    hoursInDay: 24,
-  };
-
   @Input() set time(value: string) {
     this.countDownTime = new Date(value).getTime();
   }
 
   get seconds(): string {
-    const { milliseconds, minuteInSeconds } = this.timerNumerics;
-    const seconds = Math.floor(
-      (this.getTimeDifference() % (minuteInSeconds * milliseconds)) /
-        milliseconds
-    );
-
+    const seconds = differenceInSeconds(this.countDownTime, new Date()) % 60;
     return this.formatTime(seconds);
   }
 
   get minutes(): string {
-    const { minutesInHours, milliseconds, minuteInSeconds } =
-      this.timerNumerics;
-
-    const minutes = Math.floor(
-      (this.getTimeDifference() %
-        (minutesInHours * minuteInSeconds * milliseconds)) /
-        (minuteInSeconds * milliseconds)
-    );
-
+    const minutes = differenceInMinutes(this.countDownTime, new Date()) % 60;
     return this.formatTime(minutes);
   }
 
   get hours(): string {
-    const { minutesInHours, milliseconds, minuteInSeconds, hoursInDay } =
-      this.timerNumerics;
-
-    const hours = Math.floor(
-      (this.getTimeDifference() %
-        (hoursInDay * minutesInHours * minuteInSeconds * milliseconds)) /
-        (minutesInHours * minuteInSeconds * milliseconds)
-    );
-
+    const hours = differenceInHours(this.countDownTime, new Date()) % 24;
     return this.formatTime(hours);
   }
 
   get days(): string {
-    const { minutesInHours, milliseconds, minuteInSeconds, hoursInDay } =
-      this.timerNumerics;
-
-    const days = Math.floor(
-      this.getTimeDifference() /
-        (milliseconds * minutesInHours * minuteInSeconds * hoursInDay)
-    );
-
+    const days = differenceInDays(this.countDownTime, new Date());
     return this.formatTime(days);
-  }
-
-  private getTimeDifference(): number {
-    const currentTime = new Date().getTime();
-    return this.countDownTime - currentTime;
   }
 
   private formatTime(value: number): string {
